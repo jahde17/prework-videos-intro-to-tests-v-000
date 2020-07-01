60c222efa8c0af1bbc61125afdcd84bc21b830eb
Jahde Cook <jahdeofelia@gmail.com>
2020-07-01 22:54:40 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -8,12 +8,3 @@ describe "conversions" do
     end
   end
 end
-
-describe "conversions" do
-  describe '#ounces_to_grams' do
-    it 'given one, returns 28.3495' do
-      grams = ounces_to_grams(28.3495)
-      expect(grams).to eq(28.3495)
-    end
-  end
-end
